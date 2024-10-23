f2082fdf4bfd86f0905972e9ffee72bf56e4aa8d
Roberto Perez <robertom.perez@outlook.com>
2024-10-23 09:07:30 -0300

    correccion en la logica para abrir formulario

--- js/index.js
+++ js/index.js
@@ -1,23 +1,14 @@
 function cambiarPrimerForm() {
     const empresaId = document.getElementById("empresaId");
     const personaId = document.getElementById("personaId");
-    if (empresaId.style.display === "none") {
-        empresaId.style.display = "block";
-        personaId.style.display = "none";  
-    } else {
-        personaId.style.display = "none";       
-    }    
+    empresaId.style.display = "block";
+    personaId.style.display = "none";  
 }
 
 function cambiarSegundoForm() {
     const empresaId = document.getElementById("empresaId");
     const personaId = document.getElementById("personaId");
-    if (personaId.style.display === "none") {
-        personaId.style.display = "block";
-        empresaId.style.display = "none";  
-    } else {
-        empresaId.style.display = "none";       
-    }    
+    personaId.style.display = "block";
+    empresaId.style.display = "none";  
 }
-
 
